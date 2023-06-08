55e29617f4c8856e7c1283e4c80822d05acf05df
leopard <937388985@qq.com>
2023-06-08 23:43:12 +0800

    支持汇编

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-x86_64-linux-musl.cmake
+```sh
+# config
+cmake -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE="cmake/toolchain-x86_64-linux-musl.cmake"
+
+# build
 cmake --build build
+```
--- cmake/toolchain-x86_64-linux-musl.cmake
+++ cmake/toolchain-x86_64-linux-musl.cmake
@@ -5,6 +5,7 @@ endif()
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_ASM_COMPILER "zig" cc -target x86_64-linux-musl)
 set(CMAKE_C_COMPILER "zig" cc -target x86_64-linux-musl)
 set(CMAKE_CXX_COMPILER "zig" c++ -target x86_64-linux-musl)
 
